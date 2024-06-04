50c7b5db2e3d142a5ea9a39e7e088719eb38d018
MaShisongLab <64065760+MaShisongLab@users.noreply.github.com>
2024-06-04 22:02:38 +0800

    Update TestPackage.py

--- TestPackage.py
+++ TestPackage.py
@@ -1,14 +1,5 @@
 class TestPackage:
     
-
-    gene_num = [];
-    gene_name = [];
-    pcor_all = [];
-    pcor_sampling_num = [];
-    coexpressed_cell_num = [];
-    samples_num = [];
-    RoundNumber = [];
-    SigEdges = [];
     
     def __init__(self, round_num , dataset_name = 'na'):
         
@@ -16,6 +7,4 @@ class TestPackage:
         import pandas as pd
         import random
         import time
-        
-        print('Calculating pcor in %d iterations.' %round_num)
        
