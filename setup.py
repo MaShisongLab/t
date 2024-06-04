da610ddc1b64be0144f8f5efdb2a291e0cf6f067
MaShisongLab <64065760+MaShisongLab@users.noreply.github.com>
2024-06-04 22:03:22 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='SingleCellGGM',
+    name='Test',
     version='0.1',
     license='BSD-3-Clause',
-    author='MaShisongLab',
-    py_modules = ['SingleCellGGM'],
-    keywords='SingleCellGGM',
+    author='Test',
+    py_modules = ['Test'],
+    keywords='Test',
     install_requires=['numpy','pandas'],
 )
 
